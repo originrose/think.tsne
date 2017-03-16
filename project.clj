edc1ab5939a4d567749c339bd37ea614461fa270
Chris Nuernberger <chris@thinktopic.com>
2017-03-16 14:35:56 -0600

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject think.topic/think.tsne "0.1.0-SNAPSHOT"
+  :description "Simple bindings to the TSNE algorithm of dimensionality reduction."
+  :url "http://github.com/thinktopic/think.tsne"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
